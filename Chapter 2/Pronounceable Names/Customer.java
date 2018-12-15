8990b50f10158017d07dee0b60cba11797a51d57
David Jiang <djiang86@binghamton.edu>
2018-12-15 20:00:38 +0000

    using pronounceable names

---
+++ Chapter 2/Pronounceable Names/Customer.java
@@ -0,0 +1,9 @@
+package Chapter2.PronounceableNames;
+
+import java.util.Date;
+
+public class Customer {
+    private Date generationTimestamp;
+    private Date modificationTimestamp;
+    private final String recordId = "102";
+}
---
+++ Chapter 2/Pronounceable Names/DtaRcrd102.java
@@ -0,0 +1,9 @@
+package Chapter2.PronounceableNames;
+
+import java.util.Date;
+
+public class DtaRcrd102 {
+    private Date genymdhms;
+    private Date modymdhms;
+    private final String pszqint = "102";
+}
