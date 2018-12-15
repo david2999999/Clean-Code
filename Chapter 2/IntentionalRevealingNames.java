8ef7b3348bc3fafaace40a81b14bf5be7dbf99f2
David Jiang <djiang86@binghamton.edu>
2018-12-15 19:33:08 +0000

    better name technique

--- Chapter 2/IntentionalRevealingNames.java
+++ Chapter 2/IntentionalRevealingNames.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class IntentionalRevealingNames {
+    // difficult code to understand because of the naming
+    // 1) what are the things inside theList?
+    // 2) what is the significance of the zeroth subscript of an item in theList?
+    // 3) what is the significance of the value 4?
+    // 4) what would I use the list being returned?
     private List<int[]> theList;
 
     public List<int[]> geThem() {
@@ -16,6 +21,25 @@ public class IntentionalRevealingNames {
         return list1;
     }
 
-    
+
+    // say we are working on a minesweeper game
+    // we have a board that is a list of cells called theList, which is renamed to gameBoard
+    // each cell is represented by a single array
+    // the zeroth is the location of a status value
+    // and the status value of 4 means flagged
+    private static final int STATUS_VALUE = 0;
+    private static final int FLAGGED = 4;
+    private List<int[]> gameBoard;
+
+    public List<int[]> getFlaggedCells() {
+        List<int[]> flaggedCells = new ArrayList<>();
+
+        for (int[] cell: gameBoard) {
+            if (cell[STATUS_VALUE] == FLAGGED)
+                flaggedCells.add(cell);
+        }
+
+        return flaggedCells;
+    }
 
 }
