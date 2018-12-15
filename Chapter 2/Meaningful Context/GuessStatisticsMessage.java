9dce6b19f5517a2cc56f30a917c5373752e7a014
David Jiang <djiang86@binghamton.edu>
2018-12-15 21:20:15 +0000

    having meaningful context

---
+++ Chapter 2/Meaningful Context/GuessStatisticsMessage.java
@@ -0,0 +1,40 @@
+package Chapter2.MeaningfulContext;
+
+public class GuessStatisticsMessage {
+    private String number;
+    private String verb;
+    private String pluralModifier;
+
+    public String make(char candidate, int count) {
+        createPluralDependentMessageParts(count);
+        return String.format("There %s %s %s%s", verb, number, candidate, pluralModifier);
+    }
+
+    private void createPluralDependentMessageParts(int count) {
+        if (count == 0) {
+            thereAreNoLetters();
+        } else if (count == 1) {
+            thereIsOneLetter();
+        } else {
+            thereAreManyletters(count);
+        }
+    }
+
+    private void thereAreManyletters(int count) {
+        number = Integer.toString(count);
+        verb = "are";
+        pluralModifier = "s";
+    }
+
+    private void thereIsOneLetter() {
+        number = "1";
+        verb = "is";
+        pluralModifier = "";
+    }
+
+    private void thereAreNoLetters() {
+        number = "no";
+        verb = "are";
+        pluralModifier = "s";
+    }
+}
