0a6898082a5dc76618d67081d02c74cd8f38ff1c
David Jiang <djiang86@binghamton.edu>
2019-02-28 17:02:37 +0000

    avoid member prefixes

---
+++ Chapter 2/Member Prefixes/Example.java
@@ -0,0 +1,14 @@
+public class Part {
+    private String m_dsc; // The textual description
+    void setName(String name) {
+        m_dsc = name;
+    }
+}
+
+public class Part {
+    String description;
+    
+    void setDescription(String description) {
+        this.description = description;
+    }
+}
