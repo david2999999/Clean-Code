909a9aba85fdf5518d889e174419d2554be2d6e6
David Jiang <djiang86@binghamton.edu>
2019-02-28 20:30:34 +0000

    extract try catch

---
+++ Chapter 3 - Functions/ExtractTryCatch.java
@@ -0,0 +1,18 @@
+public void delete(Page page) {
+    try {
+        deletePageAndAllReferences(page);
+    }
+    catch (Exception e) {
+        logError(e);
+    }
+}
+
+private void deletePageAndAllReferences(Page page) throws Exception {
+    deletePage(page);
+    registry.deleteReference(page.name);
+    configKeys.deleteKey(page.name.makeKey());
+}
+
+private void logError(Exception e) {
+    logger.log(e.getMessage());
+}
