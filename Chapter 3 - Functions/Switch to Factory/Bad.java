4e6caf5db39bbdf374996d392e16a2dcfaa95095
David Jiang <djiang86@binghamton.edu>
2019-02-28 18:39:22 +0000

    changing switch to factory

---
+++ Chapter 3 - Functions/Switch to Factory/Bad.java
@@ -0,0 +1,13 @@
+public Money calculatePay(Employee e)
+        throws InvalidEmployeeType {
+    switch (e.type) {
+        case COMMISSIONED:
+            return calculateCommissionedPay(e);
+        case HOURLY:
+            return calculateHourlyPay(e);
+        case SALARIED:
+            return calculateSalariedPay(e);
+        default:
+            throw new InvalidEmployeeType(e.type);
+    }
+}
---
+++ Chapter 3 - Functions/Switch to Factory/Good.java
@@ -0,0 +1,24 @@
+public abstract class Employee {
+    public abstract boolean isPayday();
+    public abstract Money calculatePay();
+    public abstract void deliverPay(Money pay);
+}
+-----------------
+public interface EmployeeFactory {
+    public Employee makeEmployee(EmployeeRecord r) throws InvalidEmployeeType;
+}
+-----------------
+public class EmployeeFactoryImpl implements EmployeeFactory {
+    public Employee makeEmployee(EmployeeRecord r) throws InvalidEmployeeType {
+        switch (r.type) {
+            case COMMISSIONED:
+                return new CommissionedEmployee(r) ;
+            case HOURLY:
+                return new HourlyEmployee(r);
+            case SALARIED:
+                return new SalariedEmploye(r);
+            default:
+                throw new InvalidEmployeeType(r.type);
+        }
+    }
+}
