ec7f8492478b6752a0fef7e580fa0ac1e53bde78
David Jiang <djiang86@binghamton.edu>
2019-03-04 11:56:02 +0000

    indentation

---
+++ Chapter 5 Formatting/Horizontal/Indentation.java
@@ -0,0 +1,32 @@
+public class FitNesseServer implements SocketServer { private FitNesseContext
+context; public FitNesseServer(FitNesseContext context) { this.context =
+context; } public void serve(Socket s) { serve(s, 10000); } public void
+serve(Socket s, long requestTimeout) { try { FitNesseExpediter sender = new
+FitNesseExpediter(s, context);
+sender.setRequestParsingTimeLimit(requestTimeout); sender.start(); }
+catch(Exception e) { e.printStackTrace(); } } }
+
+-----
+
+public class FitNesseServer implements SocketServer {
+    private FitNesseContext context;
+    
+    public FitNesseServer(FitNesseContext context) {
+        this.context = context;
+    }
+    
+    public void serve(Socket s) {
+        serve(s, 10000);
+    }
+    
+    public void serve(Socket s, long requestTimeout) {
+        try {
+            FitNesseExpediter sender = new FitNesseExpediter(s, context);
+            sender.setRequestParsingTimeLimit(requestTimeout);
+            sender.start();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
