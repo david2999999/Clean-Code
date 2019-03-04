c9e90b947adb6b5f0ea86dcc90fb5dcb85af425e
David Jiang <djiang86@binghamton.edu>
2019-03-04 12:25:57 +0000

    layer of abstraction

---
+++ Chapter 6 - Object and Data Structure/Point.java
@@ -0,0 +1,15 @@
+public class Point {
+    public double x;
+    public double y;
+}
+
+//---------------
+
+public interface Point {
+    double getX();
+    double getY();
+    void setCartesian(double x, double y);
+    double getR();
+    double getTheta();
+    void setPolar(double r, double theta);
+}
